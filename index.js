ab393fe7e0133ecba0d9e8723ad348d95ae3ab29
Neo Jiang (CSS) <v-nejian@microsoft.com>
2018-08-28 09:40:55 +0800

    read env

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var http = require('http');
+let port = process.env.PORT || 1337;
 
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
+    response.end(port);
 
 });
 
-var port = process.env.PORT || 1337;
 server.listen(port);
 
-console.log("Server running at http://localhost:%d", port);
+
